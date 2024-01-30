683bd5beb8995b63b5e3127938c2f2c5d40b56d5
Shane Barton <shanecb@hotmail.ca>
2024-01-29 21:13:25 -0500

    Changed some scaling and made a coroutine to control how long an enemy is knocked back for.

--- Assets/Scripts/Misc/Knockback.cs
+++ Assets/Scripts/Misc/Knockback.cs
@@ -6,6 +6,8 @@ public class Knockback : MonoBehaviour
 {
     public bool m_gettingKnockedBack {  get; private set; }
 
+    [SerializeField] private float m_knockbackTime = .2f;
+
     private Rigidbody2D m_rigidbody;
 
     private void Awake()
@@ -23,5 +25,18 @@ public class Knockback : MonoBehaviour
         m_gettingKnockedBack = true;
         Vector2 difference = (transform.position - damageSource.position).normalized * knockbackThrust * m_rigidbody.mass;
         m_rigidbody.AddForce(difference, ForceMode2D.Impulse);
+        StartCoroutine(KnockbackRoutine());
+    }
+
+    /// <summary>
+    /// This coroutine will ensure that entities are only knocked back briefly and not indefinitely. 
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator KnockbackRoutine()
+    {
+        yield return new WaitForSeconds(m_knockbackTime);
+        m_rigidbody.velocity = Vector2.zero;
+        m_gettingKnockedBack = false;
+
     }
 }
